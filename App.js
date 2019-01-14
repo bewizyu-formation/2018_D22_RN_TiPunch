c476510e7752e1a8af11ac4b474b8883be8540bd
Clement coxam <clement.coxam@gmail.com>
2019-01-14 17:47:18 +0100

    Commit Add Contact Component
    Ajout Objet Contact
    Variable Test
    
    Add Contact List
    
    Add Contact List to ./screen
    Add Contact to ./model
    Modification on component ContactItem
    
    Modification on Contact List
    
    Modification on ContactItem
    
    Delete App.js

--- App.js
+++ App.js
@@ -8,15 +8,23 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import { createStackNavigator, createAppContainer } from "react-navigation";
-import AddContact from './src/screen/AddContactScreen'
+import {newcontact} from './Contact';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
 type Props = {};
-class App extends Component<Props> {
+export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>{newcontact}</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
@@ -40,11 +48,3 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
-
-const AppNavigator = createStackNavigator({
-  AddContact: {
-    screen: AddContact
-  },
-});
-
-export default createAppContainer(AppNavigator);
