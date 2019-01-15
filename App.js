c2a6ad048e0b0bea4359a2d46ac62da63a73d291
Clement coxam <clement.coxam@gmail.com>
2019-01-15 15:44:28 +0100

    Fix App.js

--- App.js
+++ App.js
@@ -8,23 +8,15 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import {newcontact} from './Contact';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import AddContact from './src/screen/AddContactScreen'
 
 type Props = {};
-export default class App extends Component<Props> {
+class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>{newcontact}</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        
       </View>
     );
   }
@@ -48,3 +40,11 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+const AppNavigator = createStackNavigator({
+  AddContact: {
+    screen: AddContact
+  },
+});
+
+export default createAppContainer(AppNavigator);
