e9796888efca418008c9821ace3ed43954208d92
Amaury <amaurygranvillain17@gmail.com>
2019-01-15 16:40:34 +0100

    Réecriture du code après check ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "parser": "babel-eslint",
+    "plugins": [
+      "react-native-globals"
+    ],
+    "env": {
+      "react-native-globals/all": true
+    },
+    "rules" : {
+      "react/destructuring-assignment": 0,
+      "prefer-destructuring": 0,
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    }
+    
 };
--- src/api/APIClient.js
+++ src/api/APIClient.js
@@ -1,167 +1,218 @@
+import 'isomorphic-fetch';
+import { Alert } from 'react-native';
 
-const url = 'https://familink-api.cleverapps.io'
-let token = ''
+const url = 'https://familink-api.cleverapps.io';
+let token = '';
 
 
 export const login = async (phone, password, callback) => {
-    fetch(url + '/public/login', {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            phone: phone,
-            password: password,
-        }),
-    })
+  fetch(`${url}/public/login`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      phone,
+      password,
+    }),
+  })
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        token = data.token;
-        if(callback){
-            callback(data)
-        }
+    .then((data) => {
+      token = data.token;
+      if (callback) {
+        callback(data);
+      }
     })
-    .catch(e => {console.log(e);})
-}
+    .catch((e) => {
+      Alert.alert(
+        e,
+        [
+          { text: 'OK' },
+        ],
+        { cancelable: false },
+      );
+    });
+};
 
 export const getUser = async (callback) => {
-    fetch(url + '/secured/users', {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization' : "Bearer " + token
-        }
-    })
+  fetch(`${url}/secured/users`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        if(callback){
-            callback(data)
-        }
+    .then((data) => {
+      if (callback) {
+        callback(data);
+      }
     })
-    .catch(e => {console.log(e);})
-}
+    .catch((e) => {
+      Alert.alert(
+        e,
+        [
+          { text: 'OK' },
+        ],
+        { cancelable: false },
+      );
+    });
+};
 
 export const updateUser = async (contact, callback) => {
-    fetch(url + '/secured/users', {
-        method: 'PUT',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization' : "Bearer " + token
-        },
-        body: JSON.stringify({
-            "firstName": contact.firstName,
-            "lastName": contact.lastName,
-            "email": contact.email,
-            "profile": contact.profile
-        }),
-    })
+  fetch(`${url}/secured/users`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      firstName: contact.firstName,
+      lastName: contact.lastName,
+      email: contact.email,
+      profile: contact.profile,
+    }),
+  })
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        if(callback){
-            callback(data)
-        }
+    .then((data) => {
+      if (callback) {
+        callback(data);
+      }
     })
-    .catch(e => {console.log(e);})
-}
+    .catch((e) => {
+      Alert.alert(
+        e,
+        [
+          { text: 'OK' },
+        ],
+        { cancelable: false },
+      );
+    });
+};
 
 export const getContacts = async (callback) => {
-    fetch(url + '/secured/users/contacts', {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization' : "Bearer " + token
-        }
-    })
+  fetch(`${url}/secured/users/contacts`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        if(callback){
-            callback(data)
-        }
+    .then((data) => {
+      if (callback) {
+        callback(data);
+      }
     })
-    .catch(e => {console.log(e);})
-}
+    .catch((e) => {
+      Alert.alert(
+        e,
+        [
+          { text: 'OK' },
+        ],
+        { cancelable: false },
+      );
+    });
+};
 
 export const addContact = async (contact, callback) => {
-    fetch(url + '/secured/users/contacts', {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization' : "Bearer " + token
-        },
-        body: JSON.stringify({
-            "phone": contact.phone,
-            "firstName": contact.firstName,
-            "lastName": contact.lastName,
-            "email": contact.email,
-            "profile": contact.profile,
-            "gravatar" : contact.gravatar,
-            "isFamilinkUser" : contact.isFamilinkUser,
-            "isEmergencyUser": contact.isEmergencyUser
-        }),
-    })
+  fetch(`${url}/secured/users/contacts`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      phone: contact.phone,
+      firstName: contact.firstName,
+      lastName: contact.lastName,
+      email: contact.email,
+      profile: contact.profile,
+      gravatar: contact.gravatar,
+      isFamilinkUser: contact.isFamilinkUser,
+      isEmergencyUser: contact.isEmergencyUser,
+    }),
+  })
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        if(callback){
-            callback(data)
-        }
+    .then((data) => {
+      if (callback) {
+        callback(data);
+      }
     })
-    .catch(e => {console.log(e);})
-}
+    .catch((e) => {
+      Alert.alert(
+        e,
+        [
+          { text: 'OK' },
+        ],
+        { cancelable: false },
+      );
+    });
+};
 
 export const deleteContact = async (contact, callback) => {
-    fetch(url + '/secured/users/contacts/'+ contact.id, {
-        method: 'DELETE',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization' : "Bearer " + token
-        }
-    })
+  fetch(`${url}/secured/users/contacts/${contact.id}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        if(callback){
-            callback(data)
-        }
+    .then((data) => {
+      if (callback) {
+        callback(data);
+      }
     })
-    .catch(e => {console.log(e);})
-}
+    .catch((e) => {
+      Alert.alert(
+        e,
+        [
+          { text: 'OK' },
+        ],
+        { cancelable: false },
+      );
+    });
+};
 
 export const updateContact = async (contact, callback) => {
-    fetch(url + '/secured/users/contacts/' + contact.id, {
-        method: 'PUT',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization' : "Bearer " + token
-        },
-        body: JSON.stringify({
-            "phone": contact.phone,
-            "firstName": contact.firstName,
-            "lastName": contact.lastName,
-            "email": contact.email,
-            "profile": contact.profile,
-            "gravatar" : contact.gravatar,
-            "isFamilinkUser" : contact.isFamilinkUser,
-            "isEmergencyUser": contact.isEmergencyUser
-        }),
-    })
+  fetch(`${url}/secured/users/contacts/${contact.id}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      phone: contact.phone,
+      firstName: contact.firstName,
+      lastName: contact.lastName,
+      email: contact.email,
+      profile: contact.profile,
+      gravatar: contact.gravatar,
+      isFamilinkUser: contact.isFamilinkUser,
+      isEmergencyUser: contact.isEmergencyUser,
+    }),
+  })
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        if(callback){
-            callback(data)
-        }
+    .then((data) => {
+      if (callback) {
+        callback(data);
+      }
     })
-    .catch(e => {console.log(e);})
-}
+    .catch((e) => {
+      Alert.alert(
+        e,
+        [
+          { text: 'OK' },
+        ],
+        { cancelable: false },
+      );
+    });
+};
