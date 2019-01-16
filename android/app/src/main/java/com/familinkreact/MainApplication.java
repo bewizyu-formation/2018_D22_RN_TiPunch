6ab63c47c275bd614119611ba8039a54fa80ae0c
Amaury <amaurygranvillain17@gmail.com>
2019-01-16 10:56:39 +0100

    Importer une image via une url lors de la création d'un contact

--- android/app/src/main/java/com/familinkreact/MainApplication.java
+++ android/app/src/main/java/com/familinkreact/MainApplication.java
@@ -3,6 +3,8 @@ package com.familinkreact;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
+import com.rnfs.RNFSPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
+            new RNFSPackage(),
             new RNGestureHandlerPackage()
       );
     }
