918297a59842dabc1b13b2df675e1a60a98414be
Bouurdelle Josselin <j.bourdelle@laposte.net>
2019-01-18 18:40:15 +0100

    mise en place du style sur la page de details

--- src/screen/DetailsContactScreen.js
+++ src/screen/DetailsContactScreen.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import {
-  View, Image, TextInput, Button, NetInfo
+  View, Image, TextInput, Button, NetInfo, StyleSheet
 } from 'react-native';
 import PropTypes from 'prop-types';
 import call from 'react-native-phone-call';
 import email from 'react-native-email';
 import SendSMS from 'react-native-sms'
+
+import IconIonicons from 'react-native-vector-icons/Ionicons';
+import IconFeather from 'react-native-vector-icons/Feather';
+import IconAntDesign from 'react-native-vector-icons/AntDesign';
 
 export default class DetailsContact extends Component {
   static navigationOptions = {
@@ -37,7 +41,8 @@ export default class DetailsContact extends Component {
       phone: this.contact.phone,
       email: this.contact.email,
       profile: this.contact.profile,
-      isConnect: false
+      isConnect: false,
+      iconEditSave: <IconFeather name="edit-2" style={styles.editSaveButton} size={35} color="#628B35" onPress={() => this.editContact()}/>,
     };
     this.editContact = this.editContact.bind(this);
     this.saveEdition = this.saveEdition.bind(this);
@@ -140,6 +145,7 @@ export default class DetailsContact extends Component {
       profileEditable: true,
       imageUrlEditable: true,
       buttonEditComponent: <Button onPress={() => this.saveEdition()} title="Save" />,
+      iconEditSave: <IconAntDesign name="save" style={styles.editSaveButton} size={35} color="#628B35" onPress={() => this.saveEdition()}/>
     });
   }
 
@@ -152,6 +158,7 @@ export default class DetailsContact extends Component {
       profileEditable: false,
       imageUrlEditable: false,
       buttonEditComponent: <Button onPress={() => this.editContact()} title="Modifier" />,
+      iconEditSave: <IconFeather name="edit-2" style={styles.editSaveButton} size={35} color="#628B35" onPress={() => this.editContact()}/>
     });
   }
 
@@ -161,29 +168,36 @@ export default class DetailsContact extends Component {
 
   render() {
     return (
-      <View>
-        {this.state.buttonEditComponent}
-        <Image style={{ width: 50, height: 50 }} source={{ uri: this.state.imageUrl }} />
+      <View style={styles.container}>
+        {this.state.iconEditSave}
+        <Image style={styles.profileImage} source={{ uri: this.state.imageUrl }} />
         <TextInput
           value={this.state.imageUrl}
           onChangeText={this.changeImageUrl}
           editable={this.state.imageUrlEditable}
         />
         <View>
-        <TextInput
-          id="firstNameId"
-          type="text"
-          value={this.state.firstName}
-          onChangeText={text => this.firstNameChange(text)}
-          editable={this.state.firstNameEditable}
-        />
-        <TextInput id="lastNameId" type="text" value={this.state.lastName} onChangeText={this.lastNameChange} editable={this.state.lastNameEditable} />
-        <Button onPress={() => this.callFunction()} title="Appel" />
-        <Button onPress={() => this.message()} title="Message" />
-        <Button onPress={() => this.email()} title="email" />
-        <TextInput id="phoneId" type="text" value={this.state.phone} onChangeText={this.phoneChange} editable={this.state.phoneEditable} />
-        <TextInput id="emailId" type="text" value={this.state.email} onChangeText={this.emailChange} editable={this.state.emailEditable} />
-        <TextInput id="profileId" type="text" value={this.state.profile} onChangeText={this.profileChange} editable={this.state.profileEditable} />
+          <View style={styles.nameContactContainer} >
+            <TextInput
+              style={styles.nameContact}
+              id="firstNameId"
+              type="text"
+              value={this.state.firstName}
+              onChangeText={text => this.firstNameChange(text)}
+              editable={this.state.firstNameEditable}
+            />
+            <TextInput 
+              style={styles.nameContact}id="lastNameId" type="text" value={this.state.lastName} onChangeText={this.lastNameChange} editable={this.state.lastNameEditable} />
+          </View>
+          <View style={styles.contactConainer}>
+            <IconAntDesign style={styles.callIcon} name="phone" size={35} color="#628B35" onPress={() => this.callFunction()}/>
+            <IconAntDesign style={styles.messageIcon} name="message1" size={35} color="#628B35" onPress={() => this.message()}/>
+            <IconAntDesign style={styles.mailIcon} name="mail" size={35} color="#628B35" onPress={() => this.email()}/>
+          </View>
+          
+          <TextInput id="phoneId" type="text" value={this.state.phone} onChangeText={this.phoneChange} editable={this.state.phoneEditable} />
+          <TextInput id="emailId" type="text" value={this.state.email} onChangeText={this.emailChange} editable={this.state.emailEditable} />
+          <TextInput id="profileId" type="text" value={this.state.profile} onChangeText={this.profileChange} editable={this.state.profileEditable} />
         </View>
       </View>
     );
@@ -193,3 +207,54 @@ export default class DetailsContact extends Component {
 DetailsContact.propTypes = {
   navigation: PropTypes.any.isRequired,
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    width: '100%',
+  },
+  profileImage: {
+    marginTop: 30,
+    width: 150,
+    height: 150,
+    borderRadius: 75
+  },
+  nameContact: {
+    backgroundColor: "#E2E3E7"
+  },
+  editSaveButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10
+  },
+  nameContactContainer: {
+
+  },
+  contactConainer: {
+    flexDirection: 'row',
+    width: "100%",
+    alignItems: 'center'
+  },
+  callIcon: {
+    margin:10,
+    borderRightWidth: 1,
+    borderColor: '#E5E1B8',
+  },
+  messageIcon: {
+    margin:10,
+    borderRightWidth: 1,
+    borderColor: '#E5E1B8',
+  },
+  mailIcon: {
+    margin:10,
+  },
+});
+
+/*
+Primaire Vert :  #628B35 (RVB : 98 139 53)
+Secondaire Vert : #9AC221 (RVB : 154 194 33)
+Tertiaire Vert : #CBDE6D (RVB : 203 22 109)
+Habillage Beige : #E5E1B8 (RVB : 229 225 184)
+Fond Gris clair : #E2E3E7 (RVB : 226 227 231)
+*/
