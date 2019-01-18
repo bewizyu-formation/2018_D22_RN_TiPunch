cb2022203dc9e6174f1d33f565c8e450e9fbb347
Bouurdelle Josselin <j.bourdelle@laposte.net>
2019-01-18 16:08:04 +0100

    ContactList est styleeyy !

--- src/screen/ContactList.js
+++ src/screen/ContactList.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import {
-  View, FlatList, ActivityIndicator, TouchableOpacity, NetInfo, Button
+  View, FlatList, ActivityIndicator, TouchableOpacity, NetInfo, Button, StyleSheet
 } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import ContactItem from '../component/ContactItem';
 import { login, getContacts } from '../api/APIClient';
 import {getListContactFromDatabase, setListContactInDataBase} from '../api/AsyncStorage'
+import IconEvilIcons from 'react-native-vector-icons/EvilIcons';
+import IconAntDesign from 'react-native-vector-icons/AntDesign';
+
 
 let _this = null
 
@@ -21,16 +24,8 @@ class ContactList extends Component {
     },
     headerRight: (
       <>
-        <Button 
-          onPress={() => _this.profilePress()}
-          title= "P"
-          color= "#fff">
-        </Button>
-        <Button 
-          onPress={() => _this.addButtonPress()}
-          title= "add"
-          color= "#fff">
-        </Button>
+        <IconEvilIcons name="user" style={{marginRight: 5}} size={35} color="#fff" onPress={() => _this.profilePress()}/>
+        <IconAntDesign name="plus" style={{marginLeft: 10, marginRight: 10}} size={30} color="#fff" onPress={() => _this.addButtonPress()}/>
       </>
     ),
   };
@@ -127,9 +122,11 @@ class ContactList extends Component {
 
   renderHeader = () => (
     <SearchBar
+      inputStyle={styles.searchBarInput}
+      containerStyle={styles.searchBarContainer}
       placeholder="Votre recherche ici..."
-      lightTheme
       round
+      lightTheme
       onChangeText={text => this.searchFilterFunction(text)}
       autoCorrect={false}
     />
@@ -144,19 +141,47 @@ class ContactList extends Component {
       );
     }
     return (
-      <FlatList
-        style={{ marginBottom: 10, marginTop: 20 }}
-        data={this.state.data}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={()  => this.onPressItem(item)}>
-            <ContactItem contact={item} />
-          </TouchableOpacity>
-        )}
-        keyExtractor={item => item._id}
-        ItemSeparatorComponent={this.renderSeparator}
-        ListHeaderComponent={this.renderHeader}
-      />
+      <View style={styles.container}>
+        <FlatList
+          style={styles.flatlist}
+          data={this.state.data}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={()  => this.onPressItem(item)}>
+              <ContactItem contact={item} />
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item._id}
+          ItemSeparatorComponent={this.renderSeparator}
+          ListHeaderComponent={this.renderHeader}
+          stickyHeaderIndices={[0]}
+        />
+      </View>
     );
   }
 }
 export default ContactList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    width: '100%',
+  },
+  flatlist: {
+    width: '100%'
+  },
+  searchBarInput: {
+    backgroundColor: "#E2E3E7"
+  },
+  searchBarContainer: {
+    backgroundColor: "#628B35",
+  }
+});
+
+/*
+Primaire Vert :  #628B35 (RVB : 98 139 53)
+Secondaire Vert : #9AC221 (RVB : 154 194 33)
+Tertiaire Vert : #CBDE6D (RVB : 203 22 109)
+Habillage Beige : #E5E1B8 (RVB : 229 225 184)
+Fond Gris clair : #E2E3E7 (RVB : 226 227 231)
+*/
